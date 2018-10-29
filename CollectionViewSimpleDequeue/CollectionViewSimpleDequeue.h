b6c056acadf146511b59afb0f6b113897de06cb4
Ivan Rep <ivan.rep2@gmail.com>
2018-10-29 01:10:41 +0100

    Simple dequeue

---
+++ CollectionViewSimpleDequeue/CollectionViewSimpleDequeue.h
@@ -0,0 +1,19 @@
+//
+//  CollectionViewSimpleDequeue.h
+//  CollectionViewSimpleDequeue
+//
+//  Created by Ivan Rep on 29/10/2018.
+//  Copyright © 2018 Ivan Rep. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CollectionViewSimpleDequeue.
+FOUNDATION_EXPORT double CollectionViewSimpleDequeueVersionNumber;
+
+//! Project version string for CollectionViewSimpleDequeue.
+FOUNDATION_EXPORT const unsigned char CollectionViewSimpleDequeueVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CollectionViewSimpleDequeue/PublicHeader.h>
+
+
